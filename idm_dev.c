0390ee53ada22bdffc75dc957d750149c5fbc967
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-04 11:46:21 +0300

    created device read only device
    * read/write static functions
    * open/release static functions
    * file operations structure
    * register and unregister char device

--- idm_dev.c
+++ idm_dev.c
@@ -1,15 +1,41 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
 
+#define DEVICE_NAME "idmdev"
+
+static int dev_open(struct inode*, struct file*);
+static int dev_release(struct inode*, struct file*);
+static ssize_t dev_read(struct file*, char*, size_t, loff_t*);
+struct ssize_t dev_write(struct file*, const char*, site_t, loff_t*);
+
+
+static struct file_operations fops={
+	.open = dev_open,
+	.read = dev_read,
+	.write = dev_write,
+	.release = dev_release,
+};
+
+static int major;
 
 static int __init idm_dev_init(void) {
+	major = register_chrdev(0, DEVICE_NAME, &fops);
+	
+	if (major < 0) {
+		printk(KERN_ALERT "IDM device load failed");
+		return major;
+	}
+	
 	printk(KERN_INFO "IDM device module loaded\n");
 	return 0;
 }
 
 
 static void __exit idm_dev_exit(void) {
+	unregister_chrdev(major, DEVICE_NAME);
 	printk(KERN_INFO "IDM device module unloaded\n");
 }
 
