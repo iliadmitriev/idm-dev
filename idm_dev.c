08694c2142a945f85e4dd1b02aab59bd96640a4a
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-04 12:02:56 +0300

    fix typo

--- idm_dev.c
+++ idm_dev.c
@@ -9,7 +9,7 @@
 static int dev_open(struct inode*, struct file*);
 static int dev_release(struct inode*, struct file*);
 static ssize_t dev_read(struct file*, char*, size_t, loff_t*);
-struct ssize_t dev_write(struct file*, const char*, size_t, loff_t*);
+static ssize_t dev_write(struct file*, const char*, size_t, loff_t*);
 
 
 static struct file_operations fops={
